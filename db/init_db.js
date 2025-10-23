a8805330ea53babc3b093e4f770f13434f666d5a
JosipaI <ivezic.josipa@gmail.com>
2025-10-23 15:49:50 +0200

    added session table in db

---
+++ db/init_db.js
@@ -0,0 +1,13 @@
+const createSessionTable = `
+CREATE TABLE IF NOT EXISTS "session" (
+  "sid" varchar NOT NULL COLLATE "default",
+  "sess" json NOT NULL,
+  "expire" timestamp(6) NOT NULL,
+  PRIMARY KEY ("sid")
+);
+CREATE INDEX IF NOT EXISTS "IDX_session_expire" ON "session" ("expire");
+`;
+
+db.pool.query(createSessionTable)
+  .then(() => console.log('Session table ready'))
+  .catch(err => console.error('Could not create session table:', err));
