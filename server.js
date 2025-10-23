1faa61c1c72d70ce8095e1361a9eb80ec3ab848e
JosipaI <ivezic.josipa@gmail.com>
2025-10-23 18:38:36 +0200

    expanded server.js

--- server.js
+++ server.js
@@ -2,9 +2,29 @@ const express = require('express');
 const app = express();
 const path = require('path');
 const pg = require('pg')
-const db = require('./db')
-const session = require('express-session')
-const pgSession = require('connect-pg-simple')(session)
+const passport = require('passport');
+const db = require('./db');
+const session = require('express-session');
+const pgSession = require('connect-pg-simple')(session);
+const Auth0Strategy = require('passport-auth0');
+
+require('dotenv').config();
+
+const strategy = new Auth0Strategy(
+  {
+    domain: process.env.AUTH0_DOMAIN,
+    clientID: process.env.AUTH0_CLIENT_ID,
+    clientSecret: process.env.AUTH0_CLIENT_SECRET,
+    callbackURL: '/callback'
+  },
+  (accessToken, refreshToken, extraParams, profile, done) => {
+    return done(null, profile);
+  }
+);
+
+passport.use(strategy);
+passport.serializeUser((user, done) => done(null, user));
+passport.deserializeUser((user, done) => done(null, user));
 
 const landingRouter = require('./routes/landing.routes');
 
@@ -20,11 +40,36 @@ app.use(session({
     store: new pgSession({
         pool: db.pool,
     }),
-    secret: "web2lab1-first-try",
+    secret: process.env.SESSION_SECRET,
     resave: false,
-    saveUninitialized: true
+    saveUninitialized: false
 }))
 
-app.use('/', landingRouter);
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.get('/login', passport.authenticate('auth0', {
+  scope: 'Email OpenID Profile'
+}));
+
+app.get('/callback', passport.authenticate('auth0', {
+  failureRedirect: '/'
+}), (req, res) => {
+  res.redirect('/');
+});
+
+
+app.get('/logout', (req, res) => {
+  req.logout(err => {
+    const returnTo = encodeURIComponent('https://test-demo.onrender.com');
+    const logoutURL = `https://${process.env.AUTH0_DOMAIN}/v2/logout?client_id=${process.env.AUTH0_CLIENT_ID}&returnTo=${returnTo}`;
+    res.redirect(logoutURL);
+  });
+});
+
+
+app.get('/', (req, res) => {
+  res.send(req.user ? `Welcome ${req.user.displayName}`: 'You are not logged in');
+});
 
 app.listen(3000);
