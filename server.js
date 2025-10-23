8906e9b065ea10bb538d01176225f48bc6c4384a
JosipaI <ivezic.josipa@gmail.com>
2025-10-23 18:53:24 +0200

    some server.js changes

--- server.js
+++ server.js
@@ -1,21 +1,20 @@
 const express = require('express');
-const app = express();
 const path = require('path');
-const pg = require('pg')
 const passport = require('passport');
-const db = require('./db');
 const session = require('express-session');
 const pgSession = require('connect-pg-simple')(session);
 const Auth0Strategy = require('passport-auth0');
-
+const db = require('./db');
 require('dotenv').config();
+
+const app = express();
 
 const strategy = new Auth0Strategy(
   {
     domain: process.env.AUTH0_DOMAIN,
     clientID: process.env.AUTH0_CLIENT_ID,
     clientSecret: process.env.AUTH0_CLIENT_SECRET,
-    callbackURL: '/callback'
+    callbackURL: 'https://test-demo.onrender.com/callback',
   },
   (accessToken, refreshToken, extraParams, profile, done) => {
     return done(null, profile);
@@ -26,50 +25,40 @@ passport.use(strategy);
 passport.serializeUser((user, done) => done(null, user));
 passport.deserializeUser((user, done) => done(null, user));
 
-const landingRouter = require('./routes/landing.routes');
-
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
-
-app.use(express.static(path.join(__dirname, 'public')));
-
-app.use(express.urlencoded({ extended: true }));
-
-//pohrana sjednica u postgres bazu korštenjem connect-pg-simple modula
-app.use(session({
-    store: new pgSession({
-        pool: db.pool,
-    }),
+app.use(
+  session({
+    store: new pgSession({ pool: db.pool }),
     secret: process.env.SESSION_SECRET,
     resave: false,
-    saveUninitialized: false
-}))
+    saveUninitialized: false,
+  })
+);
 
 app.use(passport.initialize());
 app.use(passport.session());
 
-app.get('/login', passport.authenticate('auth0', {
-  scope: 'Email OpenID Profile'
-}));
+// Auth routes
+app.get('/login', passport.authenticate('auth0', { scope: 'openid profile email' }));
 
-app.get('/callback', passport.authenticate('auth0', {
-  failureRedirect: '/'
-}), (req, res) => {
-  res.redirect('/');
-});
+app.get(
+  '/callback',
+  passport.authenticate('auth0', { failureRedirect: '/' }),
+  (req, res) => {
+    res.redirect('/');
+  }
+);
 
-
-app.get('/logout', (req, res) => {
+app.get('/logout', (req, res, next) => {
   req.logout(err => {
+    if (err) return next(err);
     const returnTo = encodeURIComponent('https://test-demo.onrender.com');
     const logoutURL = `https://${process.env.AUTH0_DOMAIN}/v2/logout?client_id=${process.env.AUTH0_CLIENT_ID}&returnTo=${returnTo}`;
     res.redirect(logoutURL);
   });
 });
 
-
 app.get('/', (req, res) => {
-  res.send(req.user ? `Welcome ${req.user.displayName}`: 'You are not logged in');
+  res.send(req.user ? `Welcome, ${req.user.displayName || req.user.nickname}` : 'You are not logged in');
 });
 
-app.listen(3000);
+app.listen(3000, () => console.log('Server running on port 3000'));
