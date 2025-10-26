4ce3edad29967226177d182bd5e9a0a5cb19bd42
JosipaI <ivezic.josipa@gmail.com>
2025-10-26 22:56:57 +0100

    again

--- routes/lotto.routes.js
+++ routes/lotto.routes.js
@@ -51,7 +51,7 @@ router.post('/store-results', async (req, res) => {
       ORDER BY rounds_id DESC 
       LIMIT 1
     `);
-    if (lastClosedRound.length === 0) {
+    if (lastClosedRound.rowCount === 0) {
       return res.status(400).json({ error: 'Nije zatvoreno kolo' });
     }
 
