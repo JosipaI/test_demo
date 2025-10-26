e93f7f58392a1de4f68170a99163702fa34a7cc2
JosipaI <ivezic.josipa@gmail.com>
2025-10-27 00:49:12 +0100

    w

--- routes/lotto.routes.js
+++ routes/lotto.routes.js
@@ -78,9 +78,9 @@ router.post('/store-results', async (req, res) => {
     if (round.numbers) {
       return res.status(400).json({ error: 'Rezultati ovog kola su spremljeni' });
     }
-    cpe.log("HEREEEEEEEE 1111111111111")
+    console.log("HEREEEEEEEE 1111111111111")
     const newLocal = await pool.query('UPDATE rounds SET numbers = $1 WHERE rounds_id = $2', [numbersArray.join(','), round.rounds_id]);
-    cpe.log("HEREEEEEEEE 2222222222222")
+    console.log("HEREEEEEEEE 2222222222222")
     
     res.status(204).send();
   } catch (e) {
