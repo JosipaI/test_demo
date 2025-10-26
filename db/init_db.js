badee6c3f4bc7f405950069491234e6a5b513e09
JosipaI <ivezic.josipa@gmail.com>
2025-10-26 21:10:16 +0100

    db filled

--- db/init_db.js
+++ db/init_db.js
@@ -2,13 +2,30 @@ const db = require('./index');
 
 
 const createSessionTable = `
-CREATE TABLE IF NOT EXISTS "session" (
-  "sid" varchar NOT NULL COLLATE "default",
-  "sess" json NOT NULL,
-  "expire" timestamp(6) NOT NULL,
+CREATE TABLE IF NOT EXISTS session (
+  sid varchar NOT NULL COLLATE "default",
+  sess json NOT NULL,
+  expire timestamp(6) NOT NULL,
   PRIMARY KEY ("sid")
 );
 CREATE INDEX IF NOT EXISTS "IDX_session_expire" ON "session" ("expire");
+
+CREATE TABLE IF NOT EXISTS rounds (
+  rounds_id SERIAL PRIMARY KEY,
+  is_active BOOLEAN DEFAULT FALSE,
+  numbers INTEGER[],
+  creation_time TIMESTAMP DEFAULT NOW()
+);
+
+CREATE TABLE IF NOT EXISTS tickets (
+  id SERIAL PRIMARY KEY,
+  rounds_id INTEGER REFERENCES rounds(rounds_id),
+  user_email TEXT,
+  user_id VARCHAR(20),
+  numbers TEXT,
+  uuid_code UUID DEFAULT gen_random_uuid(),
+  creation_time TIMESTAMP DEFAULT NOW()
+);
 `;
 
 db.pool.query(createSessionTable)
